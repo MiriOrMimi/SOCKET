1411b1a4f464357b1b457b27e51e47977e6367f6
meme <santimiriam01@gmail.com>
2024-05-13 16:30:55 +0200

    impostazione progetto

---
+++ Agenda.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOCKET
+{
+    internal class Agenda
+    {
+        public List<Contatto> Contatti { get; set; }
+        public Agenda()
+        {
+            Contatti = new List<Contatto> { };
+        }
+
+        public void AggiungiContatto(string nome, string ip)
+        {
+            Contatti.Add(new Contatto(nome, ip));
+        }
+    }
+
+    internal class Contatto {
+        public string Nome { get; set; }
+        public string Indirizzo { get; set; }
+        public Contatto(string n, string ip)
+        {
+            Nome = n;
+            Indirizzo = ip;
+        }
+    }
+    
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace SOCKET
     /// </summary>
     public partial class MainWindow : Window
     {
+        Agenda agenda;
         public MainWindow()
         {
             InitializeComponent();
+            agenda = new Agenda();
+            cbxAgenda.Items.Add("FACCEZ");
+            cbxAgenda.Items.Add("PANTERZ");
+            cbxAgenda.Items.Add("MIRI");
+            cbxAgenda.Items.Add("STRUMPA");
+        }
+
+        private void cbxAgenda_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show(cbxAgenda.SelectedItem as string);
+        }
+
+        private void btnAggiungiContatto_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtIp.Visibility== Visibility.Visible && txtName.Visibility == Visibility.Visible)
+            {
+                agenda.AggiungiContatto(txtName.Text, txtIp.Text);
+                cbxAgenda.Items.Add(agenda.Contatti[agenda.Contatti.Count - 1].Nome);
+                txtIp.Text = "Nome contatto";
+                txtName.Text = "Ip contatto";
+                txtIp.Visibility = Visibility.Collapsed;
+                txtName.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                txtIp.Visibility = Visibility.Visible;
+                txtName.Visibility = Visibility.Visible;
+            }
+           
         }
     }
 }
